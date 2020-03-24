35ea2a32679a040905f438de6e0a4bd0b2ca8d37
florence-sangobiyi <58138116+florence-sangobiyi@users.noreply.github.com>
2020-03-24 13:23:35 +0100

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var http = require('http');
 //create a server object:
 http.createServer(function (req, res) {
- res.write('How are you doing today'); //write a response to the client
+ res.write({
+    <!DOCTYPE html>
+    <html>
+    <body>
+    <h2>Form</h2>
+    <form action="/localhost:8080 "target method="post"></form> >
+      <input type="text" id="Name" value="Message"><br><br>
+      <input type="submit" value="Submit">
+    </form> 
+    </body>
+    </html>
+ }); //write a response to the client
  res.end(); //end the response
 }).listen(8080); //the server object listens on port 8080
